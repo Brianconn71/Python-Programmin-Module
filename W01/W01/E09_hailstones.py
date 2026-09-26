8a7b3a8eb293218e3667f767cfd6cfd957609b51
Brian <brian.connolly71@gmail.com>
2026-09-26 09:35:27 +0100

    ADD: added comments to E9

--- W01/W01/E09_hailstones.py
+++ W01/W01/E09_hailstones.py
@@ -22,14 +22,21 @@ def hailstones(n):
     >>> len(hailstones(27))
     112
     """
-    l = [n]
+    # take the input and transform to a list
+    list = [n]
+    # if input is 1 dont go into loop and just return the 1.
     while n != 1:
+        # modulo of the input divided by 2, if not 0 its odd
         if n % 2 != 0:
+            # so if odd we times by three
             n = (3 * n) + 1
         else:
+            # Otherwither wise by 2
             n = n //2
-        l.append(n)
-    return l
+        # then append, new number to the list
+        list.append(n)
+    # return the list.
+    return list
 
 
 def longest_hailstone(limit):
@@ -42,12 +49,19 @@ def longest_hailstone(limit):
     >>> longest_hailstone(1000)
     (871, 179)
     """
-    best_n = 1
+    # initialize best number of hailstones as 1, best cant be 0
+    best_number = 1
+    # initalize the best length of the sequence as 1
     best_length = len(hailstones(1))
 
-    for n in range(2, limit):
-        length = len(hailstones(n))
+    # loop through inout and start with best number as two otherwise dont enter the loop.
+    for value in range(2, limit):
+        # cureent best lenght which is the value inside the input used to get the list in hailstones function
+        length = len(hailstones(value))
+        # if current length is better than the current best
         if length > best_length:
-            best_n = n
+            # we initialize the variables as the value from the input and the length of the list we gewt basck from hailstones.
+            best_number = value
             best_length = length
-    return  best_n, best_length
+    # return the best number with the best sequence as a tuple
+    return  best_number, best_length
